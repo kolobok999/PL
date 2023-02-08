e33b92feb04e6af88ee4197611509110f23ce97f
Egor Dechenko <gog.992@gmail.com>
2023-02-08 22:09:14 +0300

    Example 4 base

---
+++ Example_004_base/Program.cs
@@ -0,0 +1,18 @@
+﻿int [] mas = new int[10];
+
+for (int i = 0; i < 10; i++) {
+    mas[i] = new Random().Next(1, 10);
+    Console.Write(mas[i] + " ");
+}
+
+int max = mas[0];
+
+for (int i = 0; i < mas.Length; i++)
+{
+    if (mas[i] > max)
+    {
+        max = mas[i];
+    }
+}
+
+Console.WriteLine("\n" + "Максимальное число: " + max);
