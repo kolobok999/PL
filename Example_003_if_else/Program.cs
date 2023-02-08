616d8b4938e6a024197456715dab56551c9b07dd
Egor Dechenko <gog.992@gmail.com>
2023-02-08 21:46:39 +0300

    Example 3 if-else

---
+++ Example_003_if_else/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Введие имя пользователя");
+string name = Console.ReadLine();
+
+if (name.ToLower() == "маша") {
+    Console.WriteLine("Ура,это Маша");
+}
+else {
+    Console.WriteLine("Привет, " + name);
+}
