a843bc3d8a3b9922f1f4dac8657820a70df92b73
Egor Dechenko <gog.992@gmail.com>
2023-02-14 00:35:06 +0300

    ДЗ четвертая задача

---
+++ HomeWork_4/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+// 5 -> 2, 4
+// 8 -> 2, 4, 6, 8
+
+Console.Write("Введите число: ");
+int a =  Convert.ToInt32(Console.ReadLine());
+System.Console.Write(a + " -> ");
+
+for (int i = 1; i <= a; i++)
+{
+    if (i % 2 == 0)
+    {
+        System.Console.Write(i + ", ");
+    }
+}
