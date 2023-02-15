39f9106cc257ea65271b4f99b813393c2d24cd92
Egor Dechenko <gog.992@gmail.com>
2023-02-15 20:54:11 +0300

    ДЗ 6-я задача

---
+++ Homeworks/Homework_6/Program.cs
@@ -0,0 +1,33 @@
+﻿// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+// 645 -> 5
+
+// 78 -> третьей цифры нет
+
+// 32679 -> 6
+
+int NumberDigits(int value)
+{
+    int i = 0;
+    while (value >= 1)
+    {
+        value /= 10;
+        i++;
+    }
+    return i;
+}
+
+System.Console.WriteLine("Введите число:");
+int a = int.Parse(Console.ReadLine());
+
+int n = NumberDigits(a);
+
+if (n <= 2)
+{
+    System.Console.WriteLine("Третьей цифры нет");
+}
+else
+{
+    int t = (a / (int) Math.Pow(10, n - 3)) % 10;
+    System.Console.WriteLine(a + " -> " + t);
+}
