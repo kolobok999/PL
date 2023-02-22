7c89a1cafcf6302c36a27cecca95eaa8e410be83
Egor Dechenko <gog.992@gmail.com>
2023-02-22 17:24:04 +0300

    ДЗ урок 3, 10-я задача

---
+++ Homeworks/HomeWork_10/Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
+using static HomeWork_10.Library;
+
+void PrintCube(int number)
+{
+    for (int i = 1; i <= number; i++)
+    {
+        System.Console.Write($"{Math.Pow(i, 3)}, ");
+    }
+}
+
+int n = ReadNumber();
+System.Console.Write($"{n} -> ");
+PrintCube(n);
