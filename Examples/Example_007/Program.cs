a9451ae16588a1f0575ecf71a44fb293d18e4146
Egor Dechenko <gog.992@gmail.com>
2023-02-22 18:49:19 +0300

    Лекция, урок 3, 7-я задача

---
+++ Examples/Example_007/Program.cs
@@ -0,0 +1,28 @@
+﻿// Сортировка массива методом выбора
+using static Example_007.Library;
+
+int[] Sort(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        int min = arr[i];
+        for (int j = i; j < arr.Length; j++)
+        {
+            if (arr[j] < min)
+            {
+                min = arr[j];
+                Swap(arr, j, i);
+            }
+        }
+    }
+    return arr;
+}
+
+int[] array = new int[30];
+
+WriteArray(array, 0, 100);
+PrintArray(array);
+
+array = Sort(array);
+System.Console.WriteLine();
+PrintArray(array);
