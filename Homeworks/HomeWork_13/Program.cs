d635a066e50b16fdb7c2c598fdccb00dcc43984c
Egor Dechenko <gog.992@gmail.com>
2023-02-22 22:35:33 +0300

    ДЗ урок 4, 13-я задача

---
+++ Homeworks/HomeWork_13/Program.cs
@@ -0,0 +1,12 @@
+﻿// Задача 29: Напишите метод, который задаёт массив из N элементов и выводит их на экран.
+
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+// 6, 1, 33 -> [6, 1, 33]
+using static HomeWork_13.Library;
+
+int n = ReadNumber();
+int [] array = new int[n];
+
+WriteArray(array, 1, 100);
+
+System.Console.WriteLine($"[ {PrintArray(array)}]");
