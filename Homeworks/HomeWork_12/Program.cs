98a2366a334ae9d44831d945e5e8dd94a386ca8b
Egor Dechenko <gog.992@gmail.com>
2023-02-22 22:24:48 +0300

    ДЗ урок 4, 12-я задача

---
+++ Homeworks/HomeWork_12/Program.cs
@@ -0,0 +1,34 @@
+﻿// Задача 27: Напишите метод, который принимает на вход число и выдаёт сумму цифр в числе.
+
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+using static HomeWork_12.Library;
+
+int NumberDigits(int value)
+{
+    int i = 0;
+    while (value >= 1)
+    {
+        value /= 10;
+        i++;
+    }
+    return i;
+}
+
+int number = ReadNumber();
+int n = NumberDigits(number);
+
+int SumDigits(int num)
+{
+    int sum = 0;
+    for (int i = 0; i < n; i++)
+    {
+        sum += (num / (int)Math.Pow(10, n - i - 1)) % 10;
+    }
+    return sum;
+}
+
+System.Console.WriteLine($"{number} -> {SumDigits(number)}");
+
