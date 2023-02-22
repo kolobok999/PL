0607b2f324924aad2500bbfb44202ce1a5a8917c
Egor Dechenko <gog.992@gmail.com>
2023-02-22 21:53:45 +0300

    ДЗ урок 4, 11-я задача

---
+++ Homeworks/HomeWork_11/Library.cs
@@ -0,0 +1,70 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HomeWork_11
+{
+    public class Library
+    {
+        public static void Swap(int[] arr, int a, int b)  // Меняет местами элеминты массива
+        {
+            int temp;
+            temp = arr[a];
+            arr[a] = arr[b];
+            arr[b] = temp;
+        }
+
+        public static void PrintArray(int[] arr) //Печатает массив
+        {
+            for (int i = 0; i < arr.Length; i++)
+            {
+                System.Console.Write(arr[i] + " ");
+            }
+            System.Console.WriteLine();
+        }
+
+        public static void WriteArray(int[] arr, int a, int b) // Заполняет массив случайными числами в промежутке [a, b)
+        {
+            for (int i = 0; i < arr.Length; i++)
+            {
+                arr[i] = new Random().Next(a, b);
+            }
+        }
+
+        public static void WriteArray(int[] arr) // Заполняет массив случайными числами в промежутке [1, 1000)
+        {
+            for (int i = 0; i < arr.Length; i++)
+            {
+                arr[i] = new Random().Next(0, 1000);
+            }
+        }
+
+        public static void WriteArray(int[] arr, int order) // Заполняет массив числами от 1 до n, где n - длина массива
+        {
+            for (int i = 0; i < arr.Length; i++)
+            {
+                arr[i] = i;
+            }
+        }
+
+        public static int ReadNumber()  //Чтение числа с клавиатуры
+        {
+            int num;
+            while (true)
+            {
+                try
+                {
+
+                    System.Console.WriteLine("Введите число");
+                    num = int.Parse(Console.ReadLine());
+                    return num;
+                }
+                catch
+                {
+                    System.Console.WriteLine("Это не число!!! \n");
+                }
+            }
+        }
+    }
+}
---
+++ Homeworks/HomeWork_11/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача 25: Напишите метод, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+// Math.Pow использовать нельзя
+
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+
+using static HomeWork_11.Library;
+
+int pow(int a, int b)
+{
+    int res = a;
+    if (b == 0) return 1;
+    for (int i = 1; i < b; i++)
+    {
+        res *= a;
+    }
+return res;
+}
+
+int a = ReadNumber();
+int b = ReadNumber();
+int p = pow(a, b);
+System.Console.WriteLine($"{a}, {b} -> {p}");
