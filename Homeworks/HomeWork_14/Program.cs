c734da925ceda1fb49493049fd38017358284826
Egor Dechenko <gog.992@gmail.com>
2023-02-24 18:02:34 +0300

    ДЗ урок 5, 14-я задача

---
+++ Homeworks/HomeWork_14/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами.
+// Напишите программу, которая покажет количество чётных чисел в массиве.
+
+// [345, 897, 568, 234] -> 2
+using static Library.Library;
+
+int EvenNumberCounter(int[] arr) //
+{
+    int count = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] % 2 == 0) count++;
+    }
+    return count;
+}
+
+int[] array = new int[20];
+WriteArray(array, 100, 1000);
+
+System.Console.WriteLine($"[ {PrintArray(array)}] -> {EvenNumberCounter(array)}");
+
