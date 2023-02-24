4c03a5d54271fb7eda90e8e83a11d9b5d2727036
Egor Dechenko <gog.992@gmail.com>
2023-02-24 18:14:49 +0300

    ДЗ урок 5, 15-я задача

---
+++ Homeworks/HomeWork_15/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача 36: Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+
+// [3, 7, 23, 12] -> 19
+// [-4, -6, 89, 6] -> 0
+using static Library.Library;
+
+int SumOfOddArrayElements(int[] arr)
+{
+    int sum = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (i % 2 != 0)
+        {
+            sum += arr[i];
+        }
+    }
+    return sum;
+}
+
+int[] array = new int[4];
+WriteArray(array);
+
+System.Console.WriteLine($"[ {PrintArray(array)}] -> {SumOfOddArrayElements(array)}");
