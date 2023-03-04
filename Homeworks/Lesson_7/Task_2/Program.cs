8855cdc9f11171a09a00c4e847cfecea5cc6a86e
Egor Dechenko <gog.992@gmail.com>
2023-03-04 21:52:50 +0300

    ДЗ 7-й урок, 2-я задача

--- Homeworks/Lesson_7/Task_2/Program.cs
+++ Homeworks/Lesson_7/Task_2/Program.cs
@@ -1,2 +1,33 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 50. Напишите программу, которая на вход принимает позиции элемента в двумерном массиве,
+// и возвращает значение этого элемента или же указание, что такого элемента нет.
+
+// Например, задан массив:
+
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+
+// 17 -> такого числа в массиве нет
+
+using static Library.Library;
+
+bool IsNumberInMatrix(int[,] matrix, int rows, int columns)
+{
+    return rows < matrix.GetLength(0) && columns < matrix.GetLength(1);
+}
+
+int[,] matrix = new int[3, 4];
+matrix = FillMatrix(matrix, 0, 100);
+PrintMatrix(matrix);
+
+int rows = ReadNumber("Введите номер строки") - 1;
+int columns = ReadNumber("Введите номер столбца") - 1;
+
+if (IsNumberInMatrix(matrix, rows, columns))
+{
+    System.Console.WriteLine($"Matrix[{rows + 1},{columns + 1}] = {matrix[rows, columns]}");
+}
+else
+{
+    System.Console.WriteLine("Такого числа нет");
+}
