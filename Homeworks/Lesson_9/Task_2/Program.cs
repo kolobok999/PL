293f3add1ce8fa578e679324517d821a00f933de
Egor Dechenko <gog.992@gmail.com>
2023-03-11 23:00:25 +0300

    ДЗ урок 9, задача 2

---
+++ Homeworks/Lesson_9/Task_2/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+using static Library.Library;
+
+int sumOfNumber(int m, int n)
+{
+    int sum = 0;
+    for (int i = m; i <= n; i++)
+    {
+        sum += i;
+    }
+    return sum;
+}
+
+int m = ReadNumber();
+int n = ReadNumber();
+
+System.Console.WriteLine($"M = {m}; N = {n} -> {sumOfNumber(m, n)}");
